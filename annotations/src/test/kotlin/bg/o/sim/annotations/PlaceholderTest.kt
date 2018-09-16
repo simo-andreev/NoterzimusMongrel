9dcbe62d2a59a32c7347e2d64da21ef0eee0949e
simo-andreev <simeon.zlatanov.andreev@gmail.com>
2018-09-16 23:06:46 +0300

    Add test packages.

---
+++ annotations/src/test/kotlin/bg/o/sim/annotations/PlaceholderTest.kt
@@ -0,0 +1,10 @@
+package bg.o.sim.annotations
+
+import org.junit.jupiter.api.Test
+
+internal class PlaceholderTest {
+
+    @Test
+    fun emptyTestShouldSucceed() {
+    }
+}
---
+++ application/src/test/kotlin/bg/o/sim/application/web/CrudApiControllerTest.kt
@@ -0,0 +1,36 @@
+package bg.o.sim.application.web
+
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+
+internal class CrudApiControllerTest {
+
+    @BeforeEach
+    fun setUp() {
+    }
+
+    @AfterEach
+    fun tearDown() {
+    }
+
+    @Test
+    fun fetchAll() {
+    }
+
+    @Test
+    fun fetch() {
+    }
+
+    @Test
+    fun update() {
+    }
+
+    @Test
+    fun save() {
+    }
+
+    @Test
+    fun delete() {
+    }
+}
