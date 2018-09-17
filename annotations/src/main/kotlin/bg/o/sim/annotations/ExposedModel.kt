85ca976d8354fcdf56803fa3d1277c4c2c9db285
simo-andreev <simeon.zlatanov.andreev@gmail.com>
2018-09-18 02:58:43 +0300

    Add extra parameter to ExposedModel annotation. It will mark whether to expose a web APi for the CRUD MongoDB API.

--- annotations/src/main/kotlin/bg/o/sim/annotations/ExposedModel.kt
+++ annotations/src/main/kotlin/bg/o/sim/annotations/ExposedModel.kt
@@ -4,11 +4,17 @@ package bg.o.sim.annotations
  * Still cool tho!
  *
  * Used by the [ApiGenerator][bg.o.sim.aprocessor.ApiGenerator] to find candidates for
- * `MongoDB` storage and REST API exposure.
+ * `MongoDB` storage and REST API exposure. To decline exposure of the CRUD calls as a web REST API,
+ * set the annotation's value parameter [exposeWebApi] to `false`.
  *
  * Intended for use on classes that inherit, directly or not, from [BaseEntity][bg.o.sim.application.web.BaseEntity]
  *
- * @param mappingRoot the first element of all api mappings/URLs for the CRUD operations that will be mapped and exposed
+ * @param exposeWebApi whether to expose the CRUD operations via web REST API. If set to `true` => the operations will
+ * be made available as network REST calls. If set to `false` the CRUD API operations will be accessible only within
+ * code, in the scope of the generated CRUD API. *Defaults to `true`!* [added in v0.2.0]
+ *
+ * @param mappingRoot the first element of all api mappings/URLs for the CRUD operations that will be mapped and exposed.
+ * *Only relevant if [exposeWebApi] is set to `true`. Ignored otherwise!*
  *
  * @sample bg.o.sim.application.model.TransactionShortform
  *
@@ -19,4 +25,4 @@ package bg.o.sim.annotations
  */
 @Target(AnnotationTarget.CLASS)
 @Retention(AnnotationRetention.SOURCE)
-annotation class ExposedModel(val mappingRoot: String)
+annotation class ExposedModel(val mappingRoot: String, val exposeWebApi: Boolean = true)
